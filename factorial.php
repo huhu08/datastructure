78085b3b3d2f444a578cbaff94dc00cbd31e917c
Huda <hudaelzubair@hotmail.com>
2024-02-21 13:57:51 +0300

    factorial leetcode

---
+++ factorial.php
@@ -0,0 +1,19 @@
+<?php 
+
+function FirstFactorial($num) {
+
+  // code goes here
+  $factorial=1;
+for($i=1;$i<=$num;$i++){
+$factorial=$factorial*$i;
+
+}
+
+  return $num;
+echo $factorial;
+
+}
+   
+
+
+?>
